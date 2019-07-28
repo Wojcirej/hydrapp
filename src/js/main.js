1ad1a2ddf6dc21827696235ccdba23458b332f54
Wojcirej <wojciech.bal@epoczta.pl>
2019-07-28 19:11:17 +0200

    Add events to buttons.

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,54 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+const buttonAdd = document.querySelector('.glass__add-js');
+const buttonRemove = document.querySelector('.glass__remove-js');
+const value = document.querySelector('.glass__counter-js');
+const key = new Date().toISOString().slice(0, 10);
+
+function isValueForTodaySet() {
+  return Boolean(localStorage.getItem(key));
+};
+
+function setInitialGlassCount() {
+  localStorage.setItem(key, 0);
+  value.innerHTML = '0';
+};
+
+function getGlassCount() {
+  return localStorage.getItem(key);
+};
+
+function insertGlassCountOnPage() {
+  value.innerHTML = getGlassCount();
+};
+
+function addGlass() {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+};
+
+function removeGlass() {
+  if (noWaterDrank()) return;
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) - 1);
+}
+
+function noWaterDrank() {
+  return getGlassCount() <= 0;
+};
+
+if (isValueForTodaySet()) {
+  insertGlassCountOnPage();
+} else {
+  setInitialGlassCount();
+};
+
+buttonAdd.addEventListener('click', event => {
+  addGlass();
+  insertGlassCountOnPage();
+});
+
+buttonRemove.addEventListener('click', event => {
+  removeGlass();
+  insertGlassCountOnPage();
+});
